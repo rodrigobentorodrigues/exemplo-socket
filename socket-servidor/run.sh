5c0051e77d45b54f8b6859bc47160ffcfc909ded
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-12-08 11:20:20 -0300

    Arquivo sh alterado

--- socket-servidor/run.sh
+++ socket-servidor/run.sh
@@ -2,4 +2,5 @@ mvn clean install
 sudo docker build -t atividade/socket .
 sudo docker stop servidor
 sudo docker rm servidor
+#sudo docker run -p 10999:10999 -d --name servidor atividade/socket
 sudo docker run -p 1234:1234 -d --name servidor atividade/socket
